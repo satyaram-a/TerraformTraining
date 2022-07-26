569dab3e3f3927640ecfc295ab0960401ce67cfd
satyaram adabala <satyaram.adabala@gmail.com>
2022-07-26 10:06:11 +0000

    added create instance.tf to the repositary

---
+++ provider.tf
@@ -0,0 +1,6 @@
+#Providing provider details in a common file
+provider "aws" {
+  access_key = "AKIA4Y73FSHEYR225C7R"
+  secret_key = "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"
+  region     = "us-east-1"
+}
