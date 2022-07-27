546785a52ffa9a9513578e8a6a3cb0b3dac1fb59
satyaram adabala <satyaram.adabala@gmail.com>
2022-07-27 07:22:30 +0000

    Corrected syntax errors in createinstance.tf

--- createinstance.tf
+++ createinstance.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "myFirstInstance" {
   count         = 3
   ami           = "ami-0cff7528ff583bf9a"
   instance_type = "t2.micro"
-  tags           {
+  tags          = {
     name = "Three demo instances"
   }
 }
