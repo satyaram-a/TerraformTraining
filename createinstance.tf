f524dc12638de5115900bfc37d67c07266921db5
satyaram-a <64388476+satyaram-a@users.noreply.github.com>
2022-07-27 14:03:43 +0530

    Create multiple ec2 syntax fix (#21)
    * modified create instance to generate multiple ec2 instances
    
    * modified as per the comment
    
    * Corrected syntax errors in createinstance.tf

--- createinstance.tf
+++ createinstance.tf
@@ -3,7 +3,8 @@ resource "aws_instance" "myFirstInstance" {
   count         = 3
   ami           = "ami-0cff7528ff583bf9a"
   instance_type = "t2.micro"
-  tags           {
+  tags          = {
+
     name = "Three demo instances"
   }
 }
