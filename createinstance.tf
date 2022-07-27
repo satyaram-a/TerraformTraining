6df2bd5fdb735cbb2428bebc5ec9ad44199bc227
satyaram-a <64388476+satyaram-a@users.noreply.github.com>
2022-07-27 11:47:41 +0530

    modified create instance to generate multiple ec2 instances (#20)
    * modified create instance to generate multiple ec2 instances
    
    * modified as per the comment

--- createinstance.tf
+++ createinstance.tf
@@ -1,5 +1,9 @@
 #Create Instance
 resource "aws_instance" "myFirstInstance" {
+  count         = 3
   ami           = "ami-0cff7528ff583bf9a"
   instance_type = "t2.micro"
+  tags           {
+    name = "Three demo instances"
+  }
 }
