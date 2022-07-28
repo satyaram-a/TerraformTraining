b2837d6a073f0eeab765c54549810a58eea537d6
satyaram adabala <satyaram.adabala@gmail.com>
2022-07-28 11:37:30 +0000

    added list variables to declare security groups

--- VariablesExplanation/createinstance.tf
+++ VariablesExplanation/createinstance.tf
@@ -1,8 +1,9 @@
-resource "aws_instance" "MyFirstInstnace" {
+resource "aws_instance" "myFirstInstance" {
   ami           = "ami-0cff7528ff583bf9a"
   instance_type = "t2.micro"
-
-  tags = {
-    Name = "demoinstnce"
+  tags          = {
+    Name = "demoinstance"
   }
-    }
+
+  security_groups = "${var.Security_Group}"
+}
