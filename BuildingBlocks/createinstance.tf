ef6758503eea0f18f7c605a77fd11a87ff76f25f
satyaram-a <64388476+satyaram-a@users.noreply.github.com>
2022-08-05 15:33:58 +0530

    Buliding blocks (#27)
    * committing code changes
    
    * Added comments
    
    Co-authored-by: Ubuntu <satya@satyaram.qdo1tozizyzu5mxym5a5ufzmze.qx.internal.cloudapp.net>

---
+++ BuildingBlocks/createinstance.tf
@@ -0,0 +1,64 @@
+# Creating custom resource
+
+resource "aws_key_pair" "levelup_key" {
+    key_name = "levelup_key"
+    public_key = file(var.PATH_TO_PUBLIC_KEY)
+}
+
+resource "aws_instance" "MyFirstInstnace" {
+  ami           = lookup(var.AMIS, var.AWS_REGION)
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.levelup_key.key_name
+  vpc_security_group_ids = [aws_security_group.webSG.id]
+
+  tags = {
+    Name = "custom_instance"
+  }
+
+  provisioner "file" {
+      source = "installNginx.sh"
+      destination = "/tmp/installNginx.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/installNginx.sh",
+      "sudo sed -i -e 's/\r$//' /tmp/installNginx.sh",  # Remove the spurious CR characters.
+      "sudo /tmp/installNginx.sh",
+    ]
+  }
+
+  connection {
+    host        = coalesce(self.public_ip, self.private_ip)
+    type        = "ssh"
+    user        = var.INSTANCE_USERNAME
+    private_key = file(var.PATH_TO_PRIVATE_KEY)
+  }
+}
+
+resource "aws_security_group" "webSG" {
+  name        = "webSG"
+  description = "Allow ssh  inbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+
+  }
+}
